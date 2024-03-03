52c84f07a70d747a8c09d155974192608fb047fb
BaronFAS <mikhail-upside@yandex.ru>
2024-03-03 12:09:09 +0300

    Add python-dotenv

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,18 +1,19 @@
-﻿from flask import Flask
+﻿import os
+
+from dotenv import load_dotenv
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_app.settings import Config
 
+load_dotenv()
+
 app = Flask(__name__)
+app.config['FLASK_APP'] = os.getenv('FLASK_APP')
+app.config['FLASK_ENV'] = os.getenv('FLASK_ENV')
+
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 
-from flask_app import (
-    constants,
-    gbq,
-    models,
-    order_app,
-    settings,
-    data_processing,
-    validators,
-)  # noqa
+
+from flask_app import (constants, gbq, models, order_app, settings, data_processing, validators,)  # noqa
