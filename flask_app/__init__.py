afaa14285677ee0560120a0480fe62e83d3db6fe
fel <fel@ubuntu3.myguest.virtualbox.org>
2024-03-04 19:39:10 +0300

    Create dockerfile

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,5 +1,6 @@
 ﻿from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from flask_app.settings import Config
 
@@ -7,6 +8,6 @@ app = Flask(__name__)
 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-
+migrate = Migrate(app, db)
 
 from flask_app import (constants, gbq, models, order_app, settings, data_processing, validators,)  # noqa
